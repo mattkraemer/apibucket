b03f527530916f1d5c7c5a2bfde12ea9db0a9abe
derksson <mkraemer000@gmail.com>
2020-07-28 11:38:50 +0200

    add json data

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,17 +5,24 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+import data from "../components/apis"
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div>
+      <ul>
+        {
+          data.map((item, index) => (
+            <li key={index}>
+              <h4>{item.title}</h4>
+              <span>{item.category}</span>
+              <p>{item.description}</p>
+            </li>
+          ))
+        }
+      </ul>
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
 

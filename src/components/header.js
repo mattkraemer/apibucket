f7bf21ba8a44a02f117794a9ebd18b70ce5abb46
derksson <mkraemer000@gmail.com>
2020-07-28 14:42:05 +0200

    header update

--- src/components/header.js
+++ src/components/header.js
@@ -2,24 +2,14 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
-const Header = ({ siteTitle }) => (
-  <header>
-    <div>
-      <h1>
-        <Link to="/">
-          {siteTitle}
-        </Link>
-      </h1>
+const Header = () => (
+  <header className="bg-white border-b">
+    <div className="container mx-auto px-6 h-16 flex items-center">
+      <Link to="/" className="text-xl">
+        <span className="font-semibold bg-gray-200 py-1 px-2">api</span><span className="py-1 px-2 bg-gray-100">pedia</span>
+      </Link>
     </div>
   </header>
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header

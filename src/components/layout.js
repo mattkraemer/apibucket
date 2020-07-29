5d6ebfbf75e6e2cab82b710d80ded55c42a51c20
derksson <mkraemer000@gmail.com>
2020-07-29 17:53:49 +0200

    layout update

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,42 +7,42 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql, Link } from "gatsby"
-
-import Header from "./header"
+import { Link } from "gatsby"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div>
-        <main>{children}</main>
-        <footer className="container mx-auto p-6">
-          <div className="border-t py-6 flex justify-between">
-            <div>
-              © {new Date().getFullYear()}&nbsp;
-              <Link to="/" className="">
-                <span className="font-bold">api</span><span>pedia</span>
-              </Link>
-            </div>
-            <div>
-              <Link to="/" className="link">
-                Contact
-              </Link>
-            </div>
+      <header className="bg-blue-900">
+        <div className="container mx-auto px-6 h-16 flex items-center justify-between">
+          <Link to="/" className="text-xl text-blue-100">
+            <span className="font-bold">api</span><span>pedia</span>
+          </Link>
+          <div className="w-full px-6">
+            <form>
+              <input type="text" className="px-4 py-2 bg-blue-800 text-blue-300 placeholder-blue-400 w-full rounded-lg" placeholder="Search for an API..." />
+            </form>
           </div>
-        </footer>
-      </div>
+          <div className="flex-shrink-0">
+            <span className="text-blue-200 opacity-75">Open Data API Library</span>
+          </div>
+        </div>
+      </header>
+      <main>{children}</main>
+      <footer className="container mx-auto p-6">
+        <div className="border-t py-6 flex justify-between">
+          <div>
+            © {new Date().getFullYear()}&nbsp;
+            <Link to="/" className="">
+              <span className="font-bold">api</span><span>pedia</span>
+            </Link>
+          </div>
+          <div>
+            <Link to="/" className="link">
+              Contact
+            </Link>
+          </div>
+        </div>
+      </footer>
     </>
   )
 }

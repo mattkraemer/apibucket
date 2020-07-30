28402128a744a3379b3be9ec708a0d53d1681bfd
derksson <mkraemer000@gmail.com>
2020-07-30 02:54:08 +0200

    context & layout updates

---
+++ src/layouts/index.js
@@ -0,0 +1,59 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React, { useState } from "react"
+import PropTypes from "prop-types"
+import { Link } from "gatsby"
+
+import { ContextProviderComponent } from "../components/context"
+import ContextConsumer from "../components/context"
+
+const Layout = ({children}) => {
+  const [ query, setQuery ] = useState();
+
+  return (
+    <ContextProviderComponent>
+      <header className="bg-blue-900">
+        <div className="container mx-auto px-6 h-16 flex items-center justify-between">
+          <Link to="/" className="text-xl text-blue-100">
+            <span className="font-bold">api</span><span>pedia</span>
+          </Link>
+          <div className="w-full px-6">
+            <form>
+              <ContextConsumer>
+                {({ data, set }) => (
+                  <input onChange={(event) => set({ query: event.currentTarget.value })} type="text" className="px-4 py-2 bg-blue-800 text-blue-300 placeholder-blue-400 w-full rounded-lg" placeholder="Search for an API..." />
+                )}
+              </ContextConsumer>
+            </form>
+          </div>
+          <div className="flex-shrink-0">
+            <span className="text-blue-200 opacity-75">Open Data API Library</span>
+          </div>
+        </div>
+      </header>
+      <main>{children}</main>
+      <footer className="container mx-auto p-6">
+        <div className="border-t py-6 flex justify-between">
+          <div>
+            © {new Date().getFullYear()}&nbsp;
+            <Link to="/" className="">
+              <span className="font-bold">api</span><span>pedia</span>
+            </Link>
+          </div>
+          <div>
+            <Link to="/" className="link">
+              Contact
+            </Link>
+          </div>
+        </div>
+      </footer>
+    </ContextProviderComponent>
+  )
+}
+
+export default Layout
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,68 +1,75 @@
 import React, { useState } from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
+import Layout from "../layouts"
 import Image from "../components/image"
 import SEO from "../components/seo"
 import Item from "../components/item"
 
 import importData from "../components/apis"
 
+import ContextConsumer from "../components/context"
+
 var moment = require('moment');
 
 const IndexPage = () => {
-  const [ data, setData ] = useState(importData);
+  const [ apidata, setApidata ] = useState(importData);
 
   const sorting = (event) => {
     const sorting = event.target.value;
     
     if(sorting == "newestFirst") {
-      setData(
+      setApidata(
         importData.slice().sort((a,b) => new Date(b.added).getTime() - new Date(a.added).getTime())
       )
     }
     if(sorting == "oldestFirst") {
-      setData(
+      setApidata(
         importData.slice().sort((a,b) => new Date(a.added).getTime() - new Date(b.added).getTime())
       )
     }
     if(sorting == "popularFirst") {
-      setData(
+      setApidata(
         importData.slice().sort((a,b) => new Date(b.added).getTime() - new Date(a.added).getTime())
       )
     }
   }
 
   return (
-    <Layout>
-      <SEO title="Home" />
-      <div className="bg-white shadow-sm">
-        <div className="container mx-auto px-6 py-4 flex">
-            <div className="flex items-center">
-              <span>{data.length} entries found.</span>
-            </div>
-            <div className="ml-auto flex items-center">
-              <span className="font-semibold mr-3 text-sm text-gray-700">Filter by</span>
-              <form>
-                <select onChange={(event) => sorting(event)} className="bg-white border bg-opacity-50 px-3 py-2 rounded-lg">
-                  <option value="newestFirst">Newest first</option>
-                  <option value="oldestFirst">Oldest first</option>
-                  <option value="popularFirst">Most popular first</option>
-                </select>
-              </form>
+    <ContextConsumer>
+      {({ data }) => (
+        <>
+          <SEO title="Home" />
+          <div className="bg-white shadow-sm">
+            <div className="container mx-auto px-6 py-4 flex">
+                <div className="flex items-center">
+                  <span>{apidata.length} entries found.</span>
+                </div>
+                <div className="ml-auto flex items-center">
+                  <span className="font-semibold mr-3 text-sm text-gray-700">Filter by</span>
+                  <form>
+                    <select onChange={(event) => sorting(event)} className="bg-white border bg-opacity-50 px-3 py-2 rounded-lg">
+                      <option value="newestFirst">Newest first</option>
+                      <option value="oldestFirst">Oldest first</option>
+                      <option value="popularFirst">Most popular first</option>
+                    </select>
+                  </form>
+                </div>
+              </div>
+          </div>
+          {data.query}
+          <div className="container mx-auto p-6">
+            <div className="grid grid-cols-12 gap-4">
+              {
+                apidata.map((item, index) => (
+                  <Item item={item} index={index} />
+                ))
+              }
             </div>
           </div>
-      </div>
-      <div className="container mx-auto p-6">
-        <div className="grid grid-cols-12 gap-4">
-          {
-            data.map((item, index) => (
-              <Item item={item} index={index} />
-            ))
-          }
-        </div>
-      </div>
-    </Layout>
+        </>
+      )}
+    </ContextConsumer>
   )
 }
 

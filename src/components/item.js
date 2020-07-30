89791e1e2d148b83d6acdb18e56ca9b32d413f1e
derksson <mkraemer000@gmail.com>
2020-07-30 14:29:28 +0200

    install react-helmet & overflow-hidden to body

--- src/components/item.js
+++ src/components/item.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react"
 import { Link } from "gatsby"
+import Helmet from "react-helmet"
 
 var moment = require('moment');
 
@@ -15,7 +16,7 @@ const Item = (props) => {
             <h4 className="font-medium truncate mt-auto">{item.title}</h4>
           </div>
           <div className="p-6">
-            <p>{item.description}</p>
+            <p className="truncate">{item.descriptionShort}</p>
             <div className="grid grid-cols-6 gap-px border-t mt-4">
               <div className="col-span-3 pt-4 leading-none">
                 <span className="block text-xs font-semibold">Category</span>
@@ -31,23 +32,30 @@ const Item = (props) => {
       </div>
       {
         open &&
-          <div key={index} className="absolute top-0 left-0 h-screen w-screen flex justify-center items-start">
-            <div className="absolute top-0 bottom-0 left-0 right-0 bg-blue-900 bg-opacity-25" onClick={() => setOpen(false)} />
-            <div className="bg-white lg:rounded shadow-xl z-10 lg:m-8 lg:max-w-4xl w-full overflow-hidden">
-              <div className="px-6 py-4 bg-gray-100 flex items-center">
-                <h4 className="text-3xl">{item.title}</h4>
-                <span className="ml-auto px-3 py-1 rounded-full bg-gray-200">{item.category}</span>
-              </div>
-              <div className="p-6">
-                <p>{item.description}</p>
-                <div className="border-t pt-6 mt-6 flex items-center">
-                  <Link to="/" className="px-5 py-2 inline-block bg-teal-500 hover:bg-teal-600 text-white font-medium rounded">
-                    Visit Website
-                  </Link>
+          <>
+            <Helmet
+              bodyAttributes={{
+                class: 'overflow-hidden'
+              }}
+            />
+            <div key={index} className="absolute top-0 left-0 bottom-0 right-0 flex justify-center items-start">
+              <div className="absolute top-0 bottom-0 left-0 right-0 bg-blue-900 bg-opacity-25" onClick={() => setOpen(false)} />
+              <div className="bg-white lg:rounded shadow-xl z-10 lg:m-8 lg:max-w-4xl w-full overflow-hidden">
+                <div className="px-6 py-4 bg-gray-100 flex items-center">
+                  <h4 className="text-3xl">{item.title}</h4>
+                  <span className="ml-auto px-3 py-1 rounded-full bg-gray-200">{item.category}</span>
+                </div>
+                <div className="p-6">
+                  <p>{item.descriptionLong}</p>
+                  <div className="border-t pt-6 mt-6 flex items-center">
+                    <Link to="/" className="px-5 py-2 inline-block bg-teal-500 hover:bg-teal-600 text-white font-medium rounded">
+                      Visit Website
+                    </Link>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
+          </>
       }
     </>
   )
